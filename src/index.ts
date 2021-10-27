2dbc12ec3f6e17e9be1cff516d4d7768014107d5
tiagosatur <tiagosatur@gmail.com>
2021-10-27 10:46:40 -0300

    Add swagger-ui-express, create /api/docs route and post user documentation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import "express-async-errors";
+import swaggerUI from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerConfig from "./swagger.json";
 import { handleErrorMidleware } from "./utils/CustomException";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerConfig));
 
 app.use("/users", usersRoutes);
 
